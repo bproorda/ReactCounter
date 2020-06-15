f340b6368c1ec5eb1d8f10a7343996c3f6c7db60
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-15 16:37:09 -0500

    added basic header

---
+++ counterapp/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from './Components/Header';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <p>
+//           UnderConstruction
+//         </p>
+//         {/* <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render() {
+    // <> is a Fragment; can't return multiple things
+    return (
+      <>
+        <Header/>
+        {/* <Main />
+        <Footer /> */}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ counterapp/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+function Header()
+{
+    return (
+        <header>
+        <h1>Welcome to React!</h1>
+        <nav>Links go here</nav>
+      </header>
+    )
+}
+export default Header;
